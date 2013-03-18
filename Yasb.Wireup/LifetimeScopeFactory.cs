7d1997491b4493c4bf60bcf60b6c9055e712b13a
ferdinando aprovitolo <derfy74@gmail.com>
2013-03-18 00:11:14 +0000

    RedisModule improvements

---
+++ Yasb.Wireup/LifetimeScopeFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+
+namespace Yasb.Wireup
+{
+    public class LifetimeScopeFactory
+    {
+        private List<ILifetimeScope> _lifetimeScopesList = new List<ILifetimeScope>();
+        private Action<object, ContainerBuilder> _registerer;
+        private ILifetimeScope _lifetimeScope;
+        public LifetimeScopeFactory(ILifetimeScope lifetimeScope, Action<object,ContainerBuilder> registerer)
+        {
+            _lifetimeScope = lifetimeScope;
+            _registerer = registerer;
+        }
+
+        public void EnsureLifetimeScopeFor(object tag) {
+            var lifetimeScope = GetLifetimeScopeFor(tag);
+            if (lifetimeScope == null)
+            {
+                lifetimeScope = _lifetimeScope.BeginLifetimeScope(tag, cb => _registerer(tag, cb));
+                _lifetimeScopesList.Add(lifetimeScope);
+            }
+        }
+
+        public ILifetimeScope GetLifetimeScopeFor(object tag)
+        {
+            return _lifetimeScopesList.FirstOrDefault(lf => lf.Tag.Equals(tag));
+        }
+    }
+}
