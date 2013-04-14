7f4d8290121a03733f958951e75556b391fc6762
ferdinando aprovitolo <derfy74@gmail.com>
2013-04-14 17:31:18 +0100

    Fixed solution issue

---
+++ Yasb.Common/Messaging/Configuration/EndPointConfiguration1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Yasb.Common.Messaging.Configuration
+{
+    public abstract class EndPointConfiguration<TConfiguration>
+    {
+        protected internal abstract IEndPoint CreateEndPoint(string endPoint);
+
+        public TConfiguration WithName(string name)
+        {
+            Built.Name=name;
+            return This;
+        }
+
+        public abstract TConfiguration This { get; }
+
+        internal IEndPoint Built { get;  set; }
+    }
+}
